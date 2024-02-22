d36c1eaf99e0e9db227bff18d69b930e53cac160
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-02-22 23:32:43 +0100

    update sample

--- examples/SetAddress/SetAddress.ino
+++ examples/SetAddress/SetAddress.ino
@@ -1,45 +1,45 @@
 #include <Arduino.h>
 #include <MycilaPZEM004Tv3.h>
 
-#define TARGET_ADDRESS 0x02
+#define TARGET_ADDRESS 0x42
 
-Mycila::PZEM pzem1;
+Mycila::PZEM pzem;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial)
     continue;
 
-  pzem1.begin(&Serial1, 27, 14);
+  pzem.begin(&Serial1, 27, 14);
 
   while (true) {
-    uint8_t address = pzem1.readAddress();
+    uint8_t address = pzem.readAddress();
     Serial.printf("Address: 0x%02X\n", address);
     if (address == TARGET_ADDRESS) {
       break;
     }
     if (address != MYCILA_PZEM_INVALID_ADDRESS)
-      pzem1.setAddress(TARGET_ADDRESS);
+      pzem.setAddress(TARGET_ADDRESS);
     delay(1000);
   }
 
-  pzem1.end();
+  pzem.end();
 
-  pzem1.begin(&Serial1, 27, 14, TARGET_ADDRESS);
+  pzem.begin(&Serial1, 27, 14, TARGET_ADDRESS);
 }
 
 void loop() {
   delay(1000);
-  if (pzem1.read()) {
-    uint8_t address = pzem1.readAddress();
+  if (pzem.read()) {
+    uint8_t address = pzem.readAddress();
     Serial.printf("Address: 0x%02X\n", address);
 
     Serial.print("Power: ");
-    Serial.print(pzem1.getPower());
+    Serial.print(pzem.getPower());
     Serial.println("W");
 
     Serial.print("Voltage: ");
-    Serial.print(pzem1.getVoltage());
+    Serial.print(pzem.getVoltage());
     Serial.println("V");
   }
 }
