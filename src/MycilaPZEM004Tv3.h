5ccaa68568b6ba20d5a6c84aff4e29fdd6c0d730
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-06-09 01:49:27 +0200

    v4.0.2

--- src/MycilaPZEM004Tv3.h
+++ src/MycilaPZEM004Tv3.h
@@ -12,10 +12,10 @@
 #include <ArduinoJson.h>
 #endif
 
-#define MYCILA_PZEM_VERSION          "4.0.1"
+#define MYCILA_PZEM_VERSION          "4.0.2"
 #define MYCILA_PZEM_VERSION_MAJOR    4
 #define MYCILA_PZEM_VERSION_MINOR    0
-#define MYCILA_PZEM_VERSION_REVISION 0
+#define MYCILA_PZEM_VERSION_REVISION 2
 
 #ifndef MYCILA_PZEM_ASYNC_CORE
 #define MYCILA_PZEM_ASYNC_CORE 0
